4f247192632d326e02fcf1649d517c0eb74a9e91
hongchunsim <chunsim015@gmail.com>
2021-04-05 09:35:39 +0900

    join page

---
+++ js/topBar.js
@@ -0,0 +1,15 @@
+/*
+const joinSpan = document.querySelector(".js-join");
+
+
+
+const joinPageOpen() = () => {
+    location.href="join.html"
+}
+
+const init() = () => {
+    joinSpan.addEventListener("click", joinPageOpen);
+}
+
+init();
+*/
